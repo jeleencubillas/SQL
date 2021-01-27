c6935f21e11392f5f18eea6b019f819792ea91b6
jeleencubillas <jpclavecilla@gmail.com>
2021-01-27 20:16:40 +0800

    Enclosed strings in single quotes

--- Activity_3/joins.sql
+++ Activity_3/joins.sql
@@ -36,16 +36,16 @@ CREATE TABLE Classrooms (
 INSERT INTO Classrooms 
     (id, student_id, section) 
 	VALUES
-    (1, 1, A),
-    (2, 2, A),
-    (3, 3, B),
-    (4, 4, C),
-    (5, 5, B),
-    (6, 6, A),
-    (7, 7, C),
-    (8, 8, B),
-    (9, 9, B),
-    (10, 10, C);
+    (1, 1, 'A'),
+    (2, 2, 'A'),
+    (3, 3, 'B'),
+    (4, 4, 'C'),
+    (5, 5, 'B'),
+    (6, 6, 'A'),
+    (7, 7, 'C'),
+    (8, 8, 'B'),
+    (9, 9, 'B'),
+    (10, 10, 'C');
 
 -- Combine two tables using...
 -- INNER JOIN
