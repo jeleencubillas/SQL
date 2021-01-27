9327b9ed371e8abc87b3a28d81c76e8d4935a444
jeleencubillas <jpclavecilla@gmail.com>
2021-01-27 09:22:29 +0800

    Added semicolon after each item

--- Activity_2/select.sql
+++ Activity_2/select.sql
@@ -1,12 +1,12 @@
 -- Use your previous table Students
 -- Display the count of all students
-SELECT COUNT(id) FROM Students
+SELECT COUNT(id) FROM Students;
 
 -- Select all students with location is Manila
-SELECT first_name, last_name FROM Students WHERE location = 'Manila' 
+SELECT first_name, last_name FROM Students WHERE location = 'Manila';
 
 -- Display the average age of all students
-SELECT AVG(age) FROM Students 
+SELECT AVG(age) FROM Students;
 
 -- Display all students by age descending order
-SELECT first_name, last_name, age FROM Students ORDER BY age DESC
+SELECT first_name, last_name, age FROM Students ORDER BY age DESC;
